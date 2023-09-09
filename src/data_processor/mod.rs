4ecea4543840fc19432a1e2c85c2c521c761df3f
Jesse Brookins <jessebrookins@protonmail.com>
2023-09-09 15:49:26 -0400

    Add template for data_processor module

--- src/data_processor/mod.rs
+++ src/data_processor/mod.rs
@@ -1,13 +1,6 @@
-use rusqlite::{params, Connection, Result};
+mod database;
+use crate::database;
 use std::collections::HashMap;
-
-// Assuming UserPreferences and AQIData are your data structures
-#[derive(Debug)]
-pub struct UserPreferences {
-    pub pm25_threshold: i32,
-    pub o3_threshold: i32,
-    pub pm10_threshold: i32,
-}
 
 #[derive(Debug)]
 pub struct AQIData {
@@ -16,59 +9,44 @@ pub struct AQIData {
     pub pm10: i32,
 }
 
-// Initialize SQLite database
-pub fn initialize_db() -> Result<()> {
-    let conn = Connection::open("air_quality_data.db")?;
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS user_prefs (
-                  id INTEGER PRIMARY KEY,
-                  pm25_threshold INTEGER NOT NULL,
-                  o3_threshold INTEGER NOT NULL,
-                  pm10_threshold INTEGER NOT NULL
-                  )",
-        params![],
-    )?;
-    Ok(())
+#[derive(Debug)]
+pub struct ForecastAQIData {
+    pub pm25: i32,
+    pub o3: i32,
+    pub pm10: i32,
 }
 
-// Save UserPreferences to SQLite database
-pub fn save_user_preferences(user_prefs: &UserPreferences) -> Result<()> {
-    let conn = Connection::open("air_quality_data.db")?;
-    conn.execute(
-        "INSERT INTO user_prefs (pm25_threshold, o3_threshold, pm10_threshold) VALUES (?1, ?2, ?3)",
-        params![user_prefs.pm25_threshold, user_prefs.o3_threshold, user_prefs.pm10_threshold],
-    )?;
-    Ok(())
-}
+pub fn process_aqi_data(user_id: i32, aqi_data: &HashMap<String, AQIData>) -> Result<(), Box<dyn std::error::Error>> {
+    let user_prefs = database::load_user_preferences(user_id)?;
 
-// Load UserPreferences from SQLite database
-pub fn load_user_preferences() -> Result<UserPreferences> {
-    let conn = Connection::open("air_quality_data.db")?;
-    let mut stmt = conn.prepare("SELECT pm25_threshold, o3_threshold, pm10_threshold FROM user_prefs WHERE id = 1")?;
-    let user_prefs = stmt.query_row(params![], |row| {
-        Ok(UserPreferences {
-            pm25_threshold: row.get(0)?,
-            o3_threshold: row.get(1)?,
-            pm10_threshold: row.get(2)?,
-        })
-    })?;
-
-    Ok(user_prefs)
-}
-
-// Function to process AQI data and check against user preferences
-pub fn process_aqi_data(aqi_data: &HashMap<String, AQIData>, user_prefs: &UserPreferences) {
     for (location, data) in aqi_data {
         if data.pm25 > user_prefs.pm25_threshold {
-            // Trigger alert for PM2.5
+            // tmp
         }
         if data.o3 > user_prefs.o3_threshold {
-            // Trigger alert for O3
+            // tmp
         }
         if data.pm10 > user_prefs.pm10_threshold {
-            // Trigger alert for PM10
+            // tmp
         }
     }
+    Ok(())
 }
 
-// Add more functions for data processing, anomaly detection, etc.
+pub fn calculate_forecast_accuracy(forecast_data: &HashMap<String, ForecastAQIData>, actual_data: &HashMap<String, AQIData>) -> HashMap<String, f64> {
+    let mut accuracy_map: HashMap<String, f64> = HashMap::new();
+
+    for (location, forecast) in forecast_data {
+        if let Some(actual) = actual_data.get(location) {
+            let pm25_accuracy = ((forecast.pm25 - actual.pm25).abs() as f64) / forecast.pm25 as f64;
+            let o3_accuracy = ((forecast.o3 - actual.o3).abs() as f64) / forecast.o3 as f64;
+            let pm10_accuracy = ((forecast.pm10 - actual.pm10).abs() as f64) / forecast.pm10 as f64;
+
+            let overall_accuracy = (pm25_accuracy + o3_accuracy + pm10_accuracy) / 3.0;
+            accuracy_map.insert(location.clone(), overall_accuracy);
+        }
+    }
+
+    accuracy_map
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod data_fetcher;
 mod data_processor;
 mod alert_manager;
+mod database;
 
 fn main() {
     println!("Air Quality Alert System");
